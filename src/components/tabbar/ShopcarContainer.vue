28ba5831568e03c431b651387ade04adce60c938
cara0672 <cara0672@163.com>
2019-09-27 22:29:34 +0800

    完成首页及组件切换效果

---
+++ src/components/tabbar/ShopcarContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>ShopcarContainer</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ShopcarContainer"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,18 @@
 import Vue from 'vue'
-import {Header, Button} from 'mint-ui'
 import app from './App.vue'
 import 'mint-ui/lib/style.css'
-import './mui-3.7.1/dist/css/mui.min.css'
+import './lib/mui-3.7.1/dist/css/mui.min.css'
+import './lib/mui-3.7.1/dist/css/icons-extra.css'
+import router from './router'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+import {Header, Button, Swipe, SwipeItem} from 'mint-ui'
 
+
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 Vue.component(Header.name, Header);
 
 var vm = new Vue({
@@ -11,5 +20,6 @@ var vm = new Vue({
     data: {
 
     },
-    render:c => c(app)
+    render:c => c(app),
+    router
 });
